ec8b68e17d57b7ef8e08cdd02dee696dd187911c
martijor000 <82613327+martijor000@users.noreply.github.com>
2022-05-14 17:43:21 -0700

    Added a webform to use as an api to call my local database and for my web application.

--- WebAPI/WebAPI/WebAPI/Controllers/HomeController.cs
+++ WebAPI/WebAPI/WebAPI/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WebAPI.Models;
 
 namespace WebAPI.Controllers
 {
@@ -11,8 +12,57 @@ namespace WebAPI.Controllers
         public ActionResult Index()
         {
             ViewBag.Title = "Home Page";
-
             return View();
+        }
+        public ActionResult CalculateList(Items model)
+        {
+            if (ModelState.IsValid)
+            {
+                AccessoryController accessoryController = new AccessoryController();
+                List<BabyAccessory> allItems = new List<BabyAccessory>(accessoryController.Get().ToList());
+                List<double?> comparePrice = new List<double?>();
+
+                for (int j = 0; j < model.GroceryList.Count; j++) 
+                {
+                    for (int i = 0; i < allItems.Count; i++)
+                    {
+                        if (allItems[i].name.ToLower().Contains(model.GroceryList[j].ToString().ToLower()))
+                        {
+                            if (model.GroceryList[j] != "")
+                            {
+                                comparePrice.Add(allItems[i].price);   
+                            }
+                            else
+                            {
+                                continue;
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Nope not found");
+                        }
+                    }
+
+                }
+                comparePrices(comparePrice);
+            }
+            return View("Index", model);
+        }
+        private void comparePrices(List<double?> compare)
+        {
+            if(compare != null)
+            {
+                for (int i = 0; i < compare.Count; i++)
+                {
+                    for(int j = 0; j < compare.Count; j++)
+                    {
+                        if (compare[i] > compare[j])
+                        {
+                            Console.WriteLine(compare[i]);
+                        }
+                    }
+                }
+            }
         }
     }
 }
